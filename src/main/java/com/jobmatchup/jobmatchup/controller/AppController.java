79d59f7ca05a29e846e714e1c2bc850d7223a248
NkgopolengGift <nkgopolenggift@gmail.com>
2024-05-09 15:18:57 +0200

    change database data 	modified:   pom.xml 	modified:   src/main/java/com/jobmatchup/jobmatchup/controller/AppController.java 	modified:   src/main/resources/application.properties

--- src/main/java/com/jobmatchup/jobmatchup/controller/AppController.java
+++ src/main/java/com/jobmatchup/jobmatchup/controller/AppController.java
@@ -9,9 +9,6 @@ import com.jobmatchup.jobmatchup.user.User;
 import com.jobmatchup.jobmatchup.user.UserRepository;
 
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 
 @Controller
